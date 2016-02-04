bddd5abb880bda9ef8bd113a9dd7f78f5b7015f7
Hakan Silfvernagel <abbhakan@gmail.com>
2016-02-04 14:16:43 +0100

    Added Prod & Test config

--- Calculator/Library/Itera/Santander.MvcCalc/Santander.MvcCalc/Controllers/HomeController.cs
+++ Calculator/Library/Itera/Santander.MvcCalc/Santander.MvcCalc/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -9,7 +10,8 @@ namespace Santander.MvcCalc.Controllers
     public class HomeController : Controller
     {
         public ActionResult Index()
-        {
+        {            
+            ViewBag.Message = ConfigurationManager.AppSettings["Environment"];
             return View();
         }
 
