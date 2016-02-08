225ad82f0608bc4fa1cbaa7661f8728a3d7489f3
Hakan Silfvernagel <abbhakan@gmail.com>
2016-02-08 15:19:10 +0100

    Test commit F

--- Calculator/Library/Itera/Santander.CalcService/Santander.CalcService/CalcService.cs
+++ Calculator/Library/Itera/Santander.CalcService/Santander.CalcService/CalcService.cs
@@ -12,7 +12,7 @@ namespace Santander.CalcService
     {
         private Timer _timer = null;
         private readonly ILog _log = LogManager.GetLogger(typeof (CalcService));
-        private string a = "e";
+        private string a = "f";
 
         public CalcService()
         {
